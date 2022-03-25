f33555642ba2570f2bc20fc88a325c1cf1aab747
laxman <lxmkntgswami@gmail.com>
2022-03-25 16:25:45 +0530

    minorFix

--- src/uiComps/ActiveMatchComp.js
+++ src/uiComps/ActiveMatchComp.js
@@ -51,7 +51,7 @@ const RenderTeamList = ({team}) => {
 
 const RenderP=({item})=>{
 
-    let ci = Number(item.start_index_rating) ?? 0, si = Number(item.start_index_rating) ?? 0;
+    let ci = isNaN(!Number(item.start_index_rating)) || 0, si = isNaN(!Number(item.start_index_rating)) || 0;
     const price = ci - si > 0 ? 50*(ci - si) : 0;
     return <p>{price}</p>
 }
